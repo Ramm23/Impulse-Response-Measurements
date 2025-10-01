cfa8e1228645798658d5aea5d2d389f4fdd99d26
Kristian Greve Lambertsen <kgl.spam123@gmail.com>
2025-10-01 20:21:53 +0200

    Add files via upload
    Fully working stft functions

--- tools/stft.m
+++ tools/stft.m
@@ -1,7 +1,8 @@
 function [X,t,f] = stft(x,fs,w,R,M)
 
-Nx = length(x); % Signal duration
-N = numel(w); % Window size
+
+Nx = length(x);         % Signal duration
+N = numel(w);           % Window size
 O = N-R; % Overlap between adjacent windows R = shifts between adjacent windows (step size of frames)
 L = ceil((Nx - O) / R); % Number of frames
 
@@ -11,11 +12,11 @@ pad_size = max(0, (O + L*R) - Nx); % max makes sure pad size is >= 0
 zero_pad = zeros(pad_size, 1);
 x_padded = [x; zero_pad];
 
-X = zeros(N, L); % N=length of 1 FFT, L=number of fft's
+X = zeros(N, L);                    % N=length of 1 FFT, L=number of fft's
 
 
 f = (0:M-1)' * (fs / M);
-t = (N/2 + (0:L-1)*R) / fs; % Uses the center of each window
+t = (N/2 + (0:L-1)*R) / fs;         % Uses the center of each window
 
 % Run through time signal and do DFT frame by frame
 for i = 0:L - 1
@@ -23,4 +24,6 @@ for i = 0:L - 1
     frame = x_padded(idx) .* w; % Apply window to the current frame
     X(:, i+1) = fft(frame, M); % Compute the M-point FFT so it zero-pads automatically
 end
+
+
 
