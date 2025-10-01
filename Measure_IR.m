78710f4ae05269eb70e7fabbe5f8a2a3aeb34502
Kristian laptop <kgl.spam123@gmail.com>
2025-10-01 21:49:18 +0200

    plotETC and measureIR finished

--- Measure_IR.m
+++ Measure_IR.m
@@ -10,16 +10,16 @@ startupHeaAudio
 
 %% User parameters
 fs = 48000;         %Hz, sampling frequency
-Tsweep = 2;         %s time for sweep           (I put 2 here)
-Tsilence = 1; %s silence following sweep        (I put 1 here)
+Tsweep = 2;         %s, time for sweep          (choose appropriate length)
+Tsilence = 1;       %s, silence following sweep (choose appropriate length)
 fstart = 5;         %Hz, Start frequency
 fstop = fs/2;       %Hz,  Stop frequency
-Tfadein = 0.1;   %s, Length of fade in 
-Tfadeout = 4/fs;       %s, Length of fade out
+Tfadein = 0.1;      %s, Length of fade in 
+Tfadeout = 4/fs;    %s, Length of fade out
 Ttotal = Tsweep + Tsilence;       %s, total time for sweep + silence
 
-channelsRec = [5 6]; %Which soundcard channels to record on
-channelsPlay = 1; % Which soundcard channel to play on
+channelsRec = [5 6];    % Which soundcard channels to record on
+channelsPlay = 1;       % Which soundcard channel to play on
 
 %% Generate sweep signal
 s_exp = genMeasSig(Tsweep,fs,fstart,fstop,Tsilence,Tfadein,Tfadeout);
@@ -28,7 +28,7 @@ s_exp = genMeasSig(Tsweep,fs,fstart,fstop,Tsilence,Tfadein,Tfadeout);
 [hinv, Hinv] = getInverse(s_exp);
 
 %% Reconvolve with measurement signal as sanity check
-d = getIR(s_exp, Hinv);
+d = getIR(s_exp, Hinv);         % Should generate a delta signal
 
 %% Init audio object for sound recording and playback
 ao = HeaAudioDSP;
@@ -42,11 +42,47 @@ disp('Measurement start.');
 y = ao.playrec(s_exp);
 disp('Measurement completed.');
 %% Recover IR
-h = getIR(y,Hinv);
+h = getIR(y, Hinv);      % IR [Nlength x Mchannel]
+
 h_norm = h ./ max(max(abs(h))); % Normalize IRs to the overall maximum
 %% Plot the ETC
 
+
+% Check dynamic range using plotETC
+plotETC(h_norm, fs);
+
+
+
+
 %% Plot the spectrograms
+
+
+% 1. Create a window for STFT
+M = 1024;                           % FFT size
+win = hann(M);                      % Create window for STFT
+R = 512;                            % Overlapping size
+
+
+% 2. Compute STFT of recorded sweep (y)
+[Y1, F1, T1] = stft(y(:,1), fs, win, R, M);     % STFT of channel 1
+[Y2, F2, T2] = stft(y(:,2), fs, win, R, M);     % STFT of channel 2
+
+% 3. Plot spectograms of recorded sweep signals (y)
+plotSTFT(T1, F1, Y1, fs, false, 100)
+plotSTFT(T2, F2, Y2, fs, false, 100)
+
+
+
+% 4. Compute STFT of estimated system IR (h)
+[H1, F1, T1] = stft(h(:,1), fs, win, R, M);     % STFT of channel 1
+[H2, F2, T2] = stft(h(:,2), fs, win, R, M);     % STFT of channel 2
+
+% 5. Plot spectograms of system IRs
+plotSTFT(T1, F1, H1, fs, false, 100)
+plotSTFT(T2, F2, H2, fs, false, 100)
+
+
+
 
 %% Save and add variables of importance
 save(sprintf('meas_%d_%d_%d_%d_%d_%d.mat',fix(clock)),'s_exp','Hinv','y','h_norm','fs');
